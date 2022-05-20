b2ad8182b418c395a8b3cc2e835b9bfc5be3c55b
Michael Thomsen <mit@google.com>
2022-05-20 11:09:19 +0200

    Dart: use latest template & official Docker image

--- community/samples/serving/helloworld-dart/bin/server.dart
+++ community/samples/serving/helloworld-dart/bin/server.dart
@@ -2,21 +2,31 @@ import 'dart:io';
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
 
-Future main() async {
-  // Find port to listen on from environment variable.
-  var port = int.tryParse(Platform.environment['PORT'] ?? '8080');
+// Configure routes.
+final _router = Router()
+  ..get('/', _rootHandler)
+  ..get('/echo/<message>', _echoHandler);
 
-  // Read $TARGET from environment variable.
-  var target = Platform.environment['TARGET'] ?? 'World';
+Response _rootHandler(Request req) {
+  return Response.ok('Hello, World!\n');
+}
 
-  Response handler(Request request) => Response.ok('Hello $target');
+Response _echoHandler(Request request) {
+  final message = request.params['message'];
+  return Response.ok('$message\n');
+}
 
-  // Serve handler on given port.
-  var server = await serve(
-    Pipeline().addMiddleware(logRequests()).addHandler(handler),
-    InternetAddress.anyIPv4,
-    port,
-  );
-  print('Serving at http://${server.address.host}:${server.port}');
+void main(List<String> args) async {
+  // Use any available host or container IP (usually `0.0.0.0`).
+  final ip = InternetAddress.anyIPv4;
+
+  // Configure a pipeline that logs requests.
+  final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+
+  // For running in containers, we respect the PORT environment variable.
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final server = await serve(handler, ip, port);
+  print('Server listening on port ${server.port}');
 }
